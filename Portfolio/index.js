28c3db16d6135b5513b58b6b33cb01213635923d
Normi029-RTHERY <rom.t95@free.fr>
2022-10-13 18:47:02 +0200

    .

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,6 +1,6 @@
 function animateOpening() {
     console.log('hey')
-    document.querySelector("main").setAttribute('style', 'animation: openingAnimation 350ms ease-in');
-    setTimeout(function(){document.querySelector("main").setAttribute('style', 'clip-path: circle(100% at 50% 50%)');}, 340)
+    document.querySelector("main").setAttribute('style', 'animation: openingAnimation 200ms ease-in');
+    setTimeout(function(){document.querySelector("main").setAttribute('style', 'clip-path: circle(100% at 50% 50%)');}, 190)
     
 }
