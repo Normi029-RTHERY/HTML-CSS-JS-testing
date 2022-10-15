68cbf5d26f60ddee74fb05ce9b530728fbb6e6d1
Normi029-RTHERY <rom.t95@free.fr>
2022-10-15 14:22:51 +0200

    reorganisation & new test

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,20 +1,21 @@
-let i = Math.floor(Math.random() * 3)
-const e = document.documentElement;
-switch (i) {
-    case 0:
-        e.style.setProperty('--wlcm-grad-l', 'rgb(0,140,230)');
-        e.style.setProperty('--wlcm-grad-r', 'rgb(140,0,230)');
-        break;
-    case 1:
-        e.style.setProperty('--wlcm-grad-l', 'rgb(0,230,140)');
-        e.style.setProperty('--wlcm-grad-r', 'rgb(140,230,0)');
-        break;
-    case 2:
-        e.style.setProperty('--wlcm-grad-l', 'rgb(230,140,0)');
-        e.style.setProperty('--wlcm-grad-r', 'rgb(230,0,140)');
-        break;
+{
+    let i = Math.floor(Math.random() * 3)
+    const e = document.documentElement;
+    switch (i) {
+        case 0:
+            e.style.setProperty('--wlcm-grad-l', 'rgb(0,140,230)');
+            e.style.setProperty('--wlcm-grad-r', 'rgb(140,0,230)');
+            break;
+        case 1:
+            e.style.setProperty('--wlcm-grad-l', 'rgb(0,230,140)');
+            e.style.setProperty('--wlcm-grad-r', 'rgb(140,230,0)');
+            break;
+        case 2:
+            e.style.setProperty('--wlcm-grad-l', 'rgb(230,140,0)');
+            e.style.setProperty('--wlcm-grad-r', 'rgb(230,0,140)');
+            break;
+    }
 }
-
 function animateOpening() {
     console.log('hey')
     document.querySelector("main").setAttribute('style', 'animation: openingAnimation 200ms ease-in');
